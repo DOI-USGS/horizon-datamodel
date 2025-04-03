5b5c1ef688900c01de3bb7270e36d72dbf5fa858
Dan Thomson <dthomson@contractor.usgs.gov>
2025-04-03 14:46:28 -0600

    HORIZON-5 Removing redundancies in documentation; Reordering fields in Dataset

--- horizon/License.py
+++ horizon/License.py
@@ -4,16 +4,12 @@ from pydantic import BaseModel, HttpUrl
 class License(BaseModel):
     """A legal document under which the resource is made available.
 
-    licenseIdentifier: str | None = None
-        A short, standardized version of the license name.
-    license: str
-        The full version of the license name or free text description of the license or rights associated with the resource.
-    licenseUri: str | None = None
-        The URI of the license.
-    licenseIdentifierScheme: str | None = None
-        The name of the license identifier scheme
-    schemeUri: str | None = None
-        The URI of the licenseIdentifierScheme
+    licenseIdentifier: A short, standardized version of the license name.
+    license: The full version of the license name or free text description of
+        the license or rights associated with the resource.
+    licenseUri: The URI of the license.
+    licenseIdentifierScheme: The name of the license identifier scheme
+    schemeUri: The URI of the licenseIdentifierScheme
     """
 
     licenseIdentifier: str | None = None
