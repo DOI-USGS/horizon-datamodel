7bb741417e55d3b9f98912528a0edf1b12da7149
Dan Thomson <dthomson@contractor.usgs.gov>
2025-04-30 09:37:54 -0600

    H-36 Renaming directories without "-"; also adding __init__.py

--- scripts/to_json_schema.py
+++ scripts/to_json_schema.py
@@ -6,5 +6,5 @@ from horizon.CatalogedResource import *
 main_model_schema = CatalogedResource.model_json_schema()
 print(json.dumps(main_model_schema, indent=2))
 
-with open("./output-formats/CatalogedResource.json", "w") as f:
+with open("./output_formats/CatalogedResource.json", "w") as f:
     f.write(json.dumps(main_model_schema, indent=2))
